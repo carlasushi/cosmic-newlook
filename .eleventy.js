b58eea2e4237683c230f034b0e94e7dd49aaeae3
Carla Paloma <cpcodes@pm.me>
2022-09-30 23:09:07 -0700

    posts 2 & 3

--- .eleventy.js
+++ .eleventy.js
@@ -4,13 +4,14 @@ const Image = require("@11ty/eleventy-img");
 //  worked ok for img optimization in ./img folder
 
 // (async () => {
-//         let url = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/02871920-0747-4f2f-9a6e-933dd1911454/Frame_1.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220930%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220930T051309Z&X-Amz-Expires=86400&X-Amz-Signature=c962fa87f41b4999ebbbf88d5b8e8a1a6815c093d082038107027e1e11ffbf60&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Frame%25201.jpg%22&x-id=GetObject"
+//         let url = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/c3a4a7ea-aa82-42a1-9a11-5ce36df735f2/Frame_3.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221001%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221001T053255Z&X-Amz-Expires=86400&X-Amz-Signature=5e96ed5d33993779757827011ec265aa36820e9f1ba5943772a48efbf7e37476&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Frame%25203.jpg%22&x-id=GetObject"
 //     let stats = await Image(url, {
 //         formats: ["avif", "jpeg"],
 //         widths: [600, 1000, 1600, 2400],
+
 //     });
 //     const html = Image.generateHTML(stats, {
-//         alt: "Honey Wood tumblers with teapot, fruits and books", // alt text is required!
+//         alt: "Tamalpais mugs on shelves", // alt text is required!
 //         sizes: "100vw",
 //         style: "content-visibility:auto", 
 //         decoding:"async",
